fd1193ac58ce580ca42fe82ebd95612494ec9659
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-05-04 20:24:17 +0900

    修正中

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,12 +1,17 @@
 class BookCommentsController < ApplicationController
 	def create
-		book = Book.find(params[:book_id])
-	    comment = BookComment.new(post_comment_params)
-	    comment.user_id = current_user.id
-	    comment.book_id = book.id
-	    comment.save
-	    redirect_to book_path(book)
-		
+		@book = Book.find(params[:book_id])
+	    @book_comment = BookComment.new(post_comment_params)
+	    @book_comment.user_id = current_user.id
+	    @book_comment.book_id = @book.id
+	    if @book_comment.save
+	       flash[:success] = "Comment was successfully created."
+	       redirect_to book_path(@book)
+	     else
+	     	@new_book = Book.new
+	     	render "/books/show"
+	     end
+       
 	end
 	def destroy
 		book_comment = BookComment.find(params[:id])
