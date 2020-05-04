8a6913b4a4fca2871a523b834441425322dba96d
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-05-04 14:42:33 +0900

    修正中

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  resources :book_comments
-  
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+  end
   root 'home#top'
   get 'home/about' => 'home#about'
 
