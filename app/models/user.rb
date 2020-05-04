9641146e14313e4c53470fc9c69d184365a3e3f8
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-05-04 13:57:23 +0900

    コメント件数追加作業

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable,:validatable
 
   has_many :books
+
+  has_many :book_coments, dependent: :destroy
+
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
+  resources :book_comments
   
   root 'home#top'
   get 'home/about' => 'home#about'
