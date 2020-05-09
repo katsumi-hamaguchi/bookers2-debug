0dc17f93461eee3037e748edd101b643920dbfd9
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-05-09 14:14:19 +0900

    住所

--- app/models/user.rb
+++ app/models/user.rb
@@ -61,6 +61,18 @@ class User < ApplicationRecord
      end
   end
 
+  #住所検索。prefecture_codeからprefecture_nameに変換するメソッド
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
 
 
 
